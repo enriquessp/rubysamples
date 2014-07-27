b2a644287bd06b7f80ddd37dc2410f935b562377
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2014-07-27 19:50:30 -0300

    Sample of chapter 2: String interpolation

--- ticket.rb
+++ ticket.rb
@@ -24,14 +24,7 @@ def ticket.price
 	return 5.50
 end
 
-print "This ticket is for "
-print ticket.event + ", at "
-print ticket.venue + ", on "
-puts ticket.date + "."
-
-puts "The performer is #{ticket.performer} ."
-
-print "The seat is "
-print ticket.seat + ", "
-print "and it costs $"
-puts "%.2f." % ticket.price
+print "This ticket is for #{ticket.event}, at #{ticket.venue}, on #{ticket.date}."+
+	"The performer is #{ticket.performer}" +
+	"The seat is #{ticket.seat}, " +
+	"and it costs $#{"%.2f" %ticket.price}"
