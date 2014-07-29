2f82ccd7f88ae794fba22b380d9f8dacf6e58c08
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2014-07-28 22:47:35 -0300

    Sample of chapter 2: Intrspoection with send and respond_to?

--- ticket.rb
+++ ticket.rb
@@ -28,3 +28,14 @@ print "This ticket is for #{ticket.event}, at #{ticket.venue}, on #{ticket.date}
 	"The performer is #{ticket.performer}" +
 	"The seat is #{ticket.seat}, " +
 	"and it costs $#{"%.2f" %ticket.price}"
+
+puts "Example of intrspection with send()"
+puts "First, insert the name of the ricket's method yout want to show"
+
+request = gets.chomp
+if ticket.respond_to?(request)
+	puts ticket.send(request)
+	puts ticket.__send__(request) # __send__ is a way to call without conflict with any existence of another pre-defined send
+else
+	puts "The name (#{request}) you insert doenst match any method of ticket"
+end
